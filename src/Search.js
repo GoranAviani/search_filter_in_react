459ee97e448f350f9e90b727249c6a0a1c690830
GoranAviani <goranaviani@gmail.com>
2022-05-12 20:23:01 +0200

    search_filter_in_react

--- src/Search.js
+++ src/Search.js
@@ -3,8 +3,16 @@ import React from "react";
 
 const Search = ({data}) => {
 
+    const formSubmit = () => {
+
+    }
+
     return(
-        <p>{data}</p>
+        <form onSubmit={formSubmit}>
+
+            <input/>
+            <button type="submit">Search</button>
+        </form>
     )
 }
 
