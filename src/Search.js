d7c3579893cf8b94470438959444d48c257da5b5
GoranAviani <goranaviani@gmail.com>
2022-05-12 21:23:28 +0200

    search_filter_in_react

--- src/Search.js
+++ src/Search.js
@@ -3,30 +3,32 @@ import React, {useState} from "react";
 
 const Search = ({data, appDataHandler}) => {
     const [userInput, setUserInput] = useState('')
-    const {filteredResult, setFilteredResult} = useState('')
 
-    const formSubmit = (e) => {
-        e.preventDefault()
-        appDataHandler(filteredResult)
+   const [filterList, setFilterList] = useState(data);
+
+  const handleSearch = (event) => {
+    setUserInput(event.target.value)
+    if (userInput === "") {
+      setFilterList(data);
+      return;
     }
-
-    const inputHandler = (e) => {
-        setUserInput(e.target.value)
-
-         const filteredData = data.filter(
-            (item) =>
-                item.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
-        );
-        setFilteredResult(filteredData)
-    }
-
-    return (
-        <form onSubmit={formSubmit}>
-            <input value={userInput} onChange={inputHandler}/>
-            <button type="submit">Search</button>
-            {filteredResult ? filteredResult : "enter text and press search"}
-        </form>
-    )
+    const filteredValues = data.filter(
+      (item) =>
+        item.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
+  return (
+    <div className="app">
+      <div>
+        Search: <input name="query" type="text" value={userInput} onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item) => (
+          <div>{item}</div> //Display each item
+        ))}
+    </div>
+  );
 }
 
 export default Search;
