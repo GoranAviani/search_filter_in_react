2e54f0caddbc772fe4b0f94d7e4a38de07e72025
GoranAviani <goranaviani@gmail.com>
2022-05-12 21:25:38 +0200

    search_filter_in_react

--- src/Search.js
+++ src/Search.js
@@ -3,38 +3,37 @@ import React, {useState} from "react";
 
 const Search = ({data, appDataHandler}) => {
     const [userInput, setUserInput] = useState('')
+    const [filterList, setFilterList] = useState(data);
 
-   const [filterList, setFilterList] = useState(data);
+    const handleSearch = (event) => {
+        setUserInput(event.target.value)
+        if (userInput === "") {
+            setFilterList(data);
+            return;
+        }
+        const filteredValues = data.filter(
+            (item) =>
+                item.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
+        );
+        setFilterList(filteredValues);
+    };
 
-  const handleSearch = (event) => {
-    setUserInput(event.target.value)
-    if (userInput === "") {
-      setFilterList(data);
-      return;
+    const submitFormHandler = (e) => {
+        e.preventDefault()
+        appDataHandler(filterList)
     }
-    const filteredValues = data.filter(
-      (item) =>
-        item.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
+
+    return (
+        <form onSubmit={submitFormHandler}>
+            <div>
+                Search: <input name="query" type="text" value={userInput} onChange={handleSearch}/>
+            </div>
+            {filterList &&
+                filterList.map((item) => (
+                    <div>{item}</div> //Display each item
+                ))}
+        </form>
     );
-    setFilterList(filteredValues);
-  };
-
-  const submitFormHandler = (e) => {
-      e.preventDefault()
-      appDataHandler(filterList)
-    }
-
-  return (
-    <form onSubmit={submitFormHandler}>
-      <div>
-        Search: <input name="query" type="text" value={userInput} onChange={handleSearch} />
-      </div>
-      {filterList &&
-        filterList.map((item) => (
-          <div>{item}</div> //Display each item
-        ))}
-    </form>
-  );
 }
 
 export default Search;
